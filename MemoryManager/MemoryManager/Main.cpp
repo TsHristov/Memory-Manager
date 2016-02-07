93fbc0197afe9fc895c7c2b182cad32c64d2769f
TsvetanHristov <tsvetanhristov@yahoo.com>
2016-02-07 22:04:26 +0200

    finished!

--- MemoryManager/MemoryManager/Main.cpp
+++ MemoryManager/MemoryManager/Main.cpp
@@ -6,71 +6,38 @@
 
 int main()
 {
-	{
-		size_t sz = 1024;
-		MemoryManager mngr(sz);
 
-		// Allocate block of size 40 bytes
-		size_t firstBlockSize = 10 * sizeof(int);
+	size_t sz = 1024;
+	MemoryManager mngr(sz);
 
-		int *pArr1 = (int*)mngr.Malloc(firstBlockSize);
+	// Allocate block of size 40 bytes
+	size_t firstBlockSize = 10 * sizeof(int);
 
-
-		//Allocate block of size 32 bytes
-		size_t secondBlockSize = 8 * sizeof(int);
-
-		int *pArr2 = (int*)mngr.Malloc(secondBlockSize);
-
-		std::cout << "Value of header of Block No1: " << mngr.GetHeader((char*)pArr1) << "\n";
-
-		std::cout << "Value of footer of Block No1: " << mngr.GetFooter((char*)pArr1) << "\n";
-
-		std::cout << "Value of header of Block No2: " << mngr.GetHeader((char*)pArr2) << "\n";
-
-		std::cout << "Value of footer of Block No2: " << mngr.GetFooter((char*)pArr2) << "\n";
+	int *pArr1 = (int*)mngr.Malloc(firstBlockSize);
 
 
+	//Allocate block of size 32 bytes
+	size_t secondBlockSize = 8 * sizeof(int);
 
-		std::cout << "Value of header of Block No1 after free: " << mngr.GetHeader((char*)pArr1) << "\n";
+	int *pArr2 = (int*)mngr.Malloc(secondBlockSize);
 
-		std::cout << "Value of footer of Block No1 after free : " << mngr.GetFooter((char*)pArr1) << "\n";
+	std::cout << "Value of header of Block No1: " << mngr.GetHeader((char*)pArr1) << "\n";
 
-		//Allocate block of size 16 bytes, should be put at the place of pArr2
-		size_t thirdBlockSize = 4 * sizeof(int);
+	std::cout << "Value of footer of Block No1: " << mngr.GetFooter((char*)pArr1) << "\n";
 
-		int *pArr3 = (int*)mngr.Malloc(thirdBlockSize);
+	std::cout << "Value of header of Block No2: " << mngr.GetHeader((char*)pArr2) << "\n";
 
-		mngr.ForwardIteratationOverFreeBlocks();
+	std::cout << "Value of footer of Block No2: " << mngr.GetFooter((char*)pArr2) << "\n";
 
-		mngr.Free((char*)pArr1);
-		
-		mngr.ForwardIteratationOverFreeBlocks();
 
-		mngr.Free((char*)pArr3);
+	//Allocate block of size 16 bytes, should be put at the place of pArr2
+	size_t thirdBlockSize = 4 * sizeof(int);
 
-		mngr.ForwardIteratationOverFreeBlocks();
+	int *pArr3 = (int*)mngr.Malloc(thirdBlockSize);
 
-		mngr.Free((char*)pArr2);
+	mngr.Free((char*)pArr1);
 
-		mngr.ForwardIteratationOverFreeBlocks();
+	mngr.ForwardIteratationOverFreeBlocks();
 
-		char * a = (char*)mngr.Malloc(100 * sizeof(char));
-
-		std::cout << "Value of header of a: " << mngr.GetHeaderRealSize((char*)a) << "\n";
-
-		std::cout << "Value of footer of a: " << mngr.GetFooterRealSize((char*)a) << "\n";
-
-		mngr.ForwardIteratationOverFreeBlocks();
-
-		mngr.Free((char*)a);
-
-		mngr.ForwardIteratationOverFreeBlocks();
-
-		/*std::cout << "Value of header of Block No3: " << mngr.GetHeader((char*)pArr3) << "\n";
-
-		std::cout << "Value of footer of Block No3: " << mngr.GetFooter((char*)pArr3) << "\n";*/
-	}
- 
-	_CrtDumpMemoryLeaks();
 	return 0;
 }
